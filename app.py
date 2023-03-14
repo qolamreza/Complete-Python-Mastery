89dc47eb4f24831c3941cd337561f6cb1d4161fc
Gholamreza Mohammadi <gmohammadi.iasbs@gmail.com>
2023-03-15 00:50:39 +0330

    Numbers

--- app.py
+++
@@ -1,9 +0,0 @@
-course = " Python programming"
-print(course.upper())
-print(course.lower())
-print(course.title())
-print(course.strip())
-print(course.find("Pro"))
-print(course.replace("p", "j"))
-print("pro" in course)
-print("swift" not in course)
