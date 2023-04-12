5f5d2125068b12e12f1e4c3dd31b6f32c47fead2
Gholamreza Mohammadi <gmohammadi.iasbs@gmail.com>
2023-04-12 11:52:56 +0430

    Exercise

--- Data Structures/app50.py
+++
@@ -1,9 +0,0 @@
-sentence = "This is a common interview"
-
-char_frequency = {}
-for char in sentence:
-    if char in char_frequency:
-        char_frequency[char] += 1
-    else:
-        char_frequency[char] = 1
-print(char_frequency)
