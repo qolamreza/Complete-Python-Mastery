970ba3a5790280b2fb32802c52a5b19d3bb83eab
Gholamreza Mohammadi <gmohammadi.iasbs@gmail.com>
2023-04-19 08:10:59 +0430

    The Method Overriding

---
+++ Data Structures/method_overriding.py
@@ -0,0 +1,29 @@
+class Animal:
+    def __init__(self):
+        print("Animal Constructor")
+        self.age = 1
+
+
+    def eat(self):
+        print("eat")
+
+# Animal: Parent, Base
+# Mammal: Child, Sub
+
+class Mammal(Animal):
+    def __init__(self):
+        print("Mammal Constructor")
+        self.weight = 2
+        super().__init__()
+
+    def walk(self):
+        print("walk")
+
+
+
+m = Mammal()
+print(m.age)
+print(m.weight)
+
+
+
