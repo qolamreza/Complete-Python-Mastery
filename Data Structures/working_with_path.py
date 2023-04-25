8633c1910424fefe3a80f0d8b543d2c52f8f5b4d
Gholamreza Mohammadi <gmohammadi.iasbs@gmail.com>
2023-04-26 01:47:39 +0430

    Working With Path

---
+++ Data Structures/working_with_path.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+path = Path("ecommerce/__init__.py")
+path.exists()
+path.is_file()
+path.is_dir()
+print(path.name)
+print(path.stem)
+print(path.suffix)
+print(path.parent)
+path = path.with_name("file.txt")
+print(path.absolute())
