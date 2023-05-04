6ed5f25a941afb1274ec0de460e9e7369a01173b
Gholamreza Mohammadi <gmohammadi.iasbs@gmail.com>
2023-05-04 19:55:57 +0430

    Working With Time Deltas

---
+++ Data Structures/working_with_time_deltas.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+dt1 = datetime(2018, 1, 1) + timedelta(days=5, seconds=1000)
+print(dt1)
+dt2 = datetime.now()
+
+
+duration = dt2 - dt1
+print(duration)
+print("days", duration.days)
+print("seconds", duration.seconds)
+print("total_seconds", duration.total_seconds())
